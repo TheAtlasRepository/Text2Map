2c8e0bda7cc47383766b1f8651a8c8362542f6fe
Henrik <89202778+HenrikRaakil@users.noreply.github.com>
2024-02-26 12:52:22 +0100

    Added LoadingScreen, updated icons for infopanel.

--- frontend/components/component/info-panel.tsx
+++ frontend/components/component/info-panel.tsx
@@ -1,5 +1,6 @@
 
 import { useState, useEffect } from "react";
+import { CloseIcon, TrashHeroIcon } from "../ui/icons";
 
 export default function InfoPanel(props: { 
   title: string;
@@ -24,9 +25,15 @@ export default function InfoPanel(props: {
 
   return (
       <div className="block min-w-[180px] bg-white rounded-lg shadow dark:bg-gray-800">
-        <a href="#">
-            <img className="rounded-t-lg" src={imageLink}/>
-        </a>
+        {
+            imageLink == "" ? (
+                <div style={{width:"240px", height:"125px"}}>
+                    <p className="text-center">Loading...</p>
+                </div>
+            ) : (
+                <img className="rounded-t-lg" style={{width:"240px", height:"125px"}} src={imageLink} />
+            )
+        }
         <div className="p-5">
             <a href="#">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.title}</h5>
@@ -34,57 +41,14 @@ export default function InfoPanel(props: {
         </div>
         <div className="absolute top-0 left-0 mt-2 ml-2">
         <button className="bg-gray-100 hover:bg-gray-200 rounded-full p-2 focus:outline-none">
-          <TrashIcon className="h-6 w-6 text-black" />
+          <TrashHeroIcon/>
         </button>
       </div>
       <div className="absolute top-0 right-0 mt-2 mr-2">
         <button className="bg-gray-100 hover:bg-gray-200 rounded-full p-2 focus:outline-none" onClick={props.onClosed}>
-          <XIcon className="h-6 w-6 text-black" />
+          <CloseIcon/>
         </button>
       </div>
     </div>
-  
-  )
-}
-
-function TrashIcon(props:any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M3 6h18" />
-      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2v2" />
-    </svg>
-  )
-}
-
-
-function XIcon(props:any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 24"
-      fill="currentColor"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path xmlns="http://www.w3.org/2000/svg" d="M3 21.32L21 3.32001" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-      <path xmlns="http://www.w3.org/2000/svg" d="M3 3.32001L21 21.32" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>
   )
 }
