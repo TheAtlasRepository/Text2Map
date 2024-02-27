2102a63c7763c9e9c61063f7c80b13030608f357
Henrik <89202778+HenrikRaakil@users.noreply.github.com>
2024-02-27 13:51:58 +0100

    Streamlined code
    Co-authored-by: GlennJoakimB <89195051+GlennJoakimB@users.noreply.github.com>

--- frontend/components/component/startDataSourceInput.tsx
+++ frontend/components/component/startDataSourceInput.tsx
@@ -196,7 +196,7 @@ export default function StartDataSource() {
                                     <div className="flex items-center justify-center w-full mb-2">
                                         <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-4 border-blue-200 border-dashed rounded-lg cursor-pointer hover:bg-gray-100">
                                             <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                                <UploadIcon className="w-8 h-8 mb-4 text-blue-300 " />
+                                                <UploadIcon className="w-8 h-8 mb-4 text-blue-300" />
                                                 <p className="mb-2 text-sm text-gray-400 "><span className="font-semibold">Click to upload</span> or drag and drop</p>
                                                 <p className="text-xs text-gray-400 ">CSV</p>
                                             </div>
