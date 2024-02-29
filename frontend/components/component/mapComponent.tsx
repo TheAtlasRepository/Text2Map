6121ef988c5159ba6f3e7c4c738a97bf02eb9113
Glenn Joakim Bakklund <89195051+GlennJoakimB@users.noreply.github.com>
2024-02-29 14:16:30 +0100

    B gjb error fix style load (#20)
    * Potential fix
    * Cleanup
    * Fewer errors
    
    - Removing some comments and spaces
    - Check for if geojson is given, to prevent errors on undefined

--- frontend/components/component/mapComponent.tsx
+++ frontend/components/component/mapComponent.tsx
@@ -1,81 +1,119 @@
-// MapComponent.tsx
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ReactMapGL, { Marker, Popup, Source, Layer } from 'react-map-gl';
+import type { FillLayer } from 'react-map-gl';
 import Coordinate from '../functions/Coordinates';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import InfoPanel from '../component/info-panel';
 
+/**
+ * Input props for the map component
+ */
 type MapComponentProps = {
-  markers: { latitude: number; longitude: number; type: string }[];
-  centerCoordinates: [number, number] | null;
-  initialViewState: any;
-  mapRef: React.MutableRefObject<null>;
-  selectedMarkerIndex: number | null;
-  setSelectedMarkerIndex: React.Dispatch<React.SetStateAction<number | null>>;
-  geojsonData: any;
+    markers: { latitude: number; longitude: number; type: string }[];
+    centerCoordinates: [number, number] | null;
+    initialViewState: any;
+    mapRef: React.MutableRefObject<null>;
+    selectedMarkerIndex: number | null;
+    setSelectedMarkerIndex: React.Dispatch<React.SetStateAction<number | null>>;
+    geojsonData?: any;
 };
 
-const MapComponent: React.FC<MapComponentProps> = ({
-  markers,
-  centerCoordinates,
-  initialViewState,
-  mapRef,
-  selectedMarkerIndex,
-  setSelectedMarkerIndex,
-  geojsonData,
-}) => {
-  return (
-    <ReactMapGL
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-      mapStyle="mapbox://styles/mapbox/standard"
-      initialViewState={initialViewState}
-      maxZoom={20}
-      minZoom={2}
-      ref={mapRef}
-    >
 
-      {/* Render GeoJSON */}
-      <Source id="selectedCountries" type="geojson" data={geojsonData}>
-        <Layer
-          id="selectedCountries"
-          type="fill"
-          paint={{
-            'fill-color': '#088',
-            'fill-opacity': 0.4,
-          }}
-        />
-      </Source>
-      {/* Render markers */}
-      {markers.map((marker, index) => (
-        <Marker
-          key={index}
-          latitude={marker.latitude}
-          longitude={marker.longitude}
+/**
+ * Map Component for displaying map with added formatting
+ * 
+ * @param markers 
+ * @param centerCoordinates 
+ * @param initialViewState 
+ * @param mapRef 
+ * @param selectedMarkerIndex 
+ * @param setSelectedMarkerIndex 
+ * @param geojsonData 
+ * @returns 
+ */
+const MapComponent: React.FC<MapComponentProps> = ({
+    markers,
+    centerCoordinates,
+    initialViewState,
+    mapRef,
+    selectedMarkerIndex,
+    setSelectedMarkerIndex,
+    geojsonData,
+}) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [gotGeoJson, setGotGeoJsonState] = useState(false);
+
+    const handleOnLoad = () => {
+        setIsLoaded(true);
+    };
+
+    useEffect(() => {
+        if (geojsonData != undefined && !gotGeoJson) {
+            setGotGeoJsonState(true);
+        }
+    })
+
+    return (
+        <ReactMapGL
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+            mapStyle="mapbox://styles/mapbox/standard"
+            initialViewState={initialViewState}
+            maxZoom={20}
+            minZoom={2}
+            ref={mapRef}
+            onLoad={handleOnLoad}
         >
-          <Coordinate
-            latitude={marker.latitude}
-            longitude={marker.longitude}
-            type={marker.type}
-            isSelected={selectedMarkerIndex === index}
-            onClick={() => setSelectedMarkerIndex(index)}
-          />
-          {selectedMarkerIndex === index && (
-            <Popup
-              latitude={marker.latitude}
-              longitude={marker.longitude}
-              closeButton={false}
-              closeOnClick={false}
-              className="custom-popup"
-              anchor="bottom"
-              offset={[0, -30] as [number, number]}
-            >
-              <InfoPanel title={marker.type} onClosed={() => setSelectedMarkerIndex(null)} />
-            </Popup>
-          )}
-        </Marker>
-      ))}
-    </ReactMapGL>
-  );
+
+
+            {isLoaded &&
+                <>
+                    {/* Render GeoJSON */}
+                    {gotGeoJson &&
+                    <Source id="selectedCountries" type="geojson" data={geojsonData}>
+                        <Layer
+                            id="selectedCountries"
+                            type="fill"
+                            paint={{
+                                'fill-color': '#088',
+                                'fill-opacity': 0.4,
+                            }}
+                        />
+                    </Source>
+                    }
+
+                    {/* Render markers */}
+                    {markers.map((marker, index) => (
+                        <Marker
+                            key={index}
+                            latitude={marker.latitude}
+                            longitude={marker.longitude}
+                        >
+                            <Coordinate
+                                latitude={marker.latitude}
+                                longitude={marker.longitude}
+                                type={marker.type}
+                                isSelected={selectedMarkerIndex === index}
+                                onClick={() => setSelectedMarkerIndex(index)}
+                            />
+                            {selectedMarkerIndex === index && (
+                                <Popup
+                                    latitude={marker.latitude}
+                                    longitude={marker.longitude}
+                                    closeButton={false}
+                                    closeOnClick={false}
+                                    className="custom-popup"
+                                    anchor="bottom"
+                                    offset={[0, -30] as [number, number]}
+                                >
+                                    <InfoPanel title={marker.type} onClosed={() => setSelectedMarkerIndex(null)} />
+                                </Popup>
+                            )}
+                        </Marker>
+                    ))}
+                </>
+            }
+        </ReactMapGL>
+    );
 };
 
 export default MapComponent;
