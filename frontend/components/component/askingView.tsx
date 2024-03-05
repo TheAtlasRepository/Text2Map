929972db312ac0f40000bf58c99e4b23776dc6c6
Glenn Joakim Bakklund <89195051+GlennJoakimB@users.noreply.github.com>
2024-03-05 09:14:22 +0100

    Quick fix (#24)
    - Fixed mapview overflow

--- frontend/components/component/askingView.tsx
+++ frontend/components/component/askingView.tsx
@@ -100,7 +100,7 @@ export default function AskingView({ onEditSave, editedText }: { onEditSave: (te
     };    
 
     return (
-      <div className="bg-white min-h-screen overflow-y-auto">
+      <div className="bg-white overflow-y-auto">
         {/* <header className="flex items-center justify-between p-4 border-b">
           <div className="flex items-center space-x-4">
             <h1 className="text-lg font-semibold">Unsaved map</h1>
