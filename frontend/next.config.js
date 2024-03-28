51f14ea30775f696366e60b40cf35f72670147fc
Eirik Bjørdal <eiriksbjordal@gmail.com>
2024-03-28 12:50:49 +0800

    frotnend fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+const path = require('path');
+
+module.exports = {
+ webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+ },
+};
+
 module.exports = nextConfig
